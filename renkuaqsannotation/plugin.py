71f784b25c9f36eaa67c4d3495e201bd74f57474
burnout87 <gabriele.barni@gmail.com>
2023-07-19 15:52:40 +0200

    license

--- renkuaqsannotation/plugin.py
+++ renkuaqsannotation/plugin.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright 2020 - Gabriele Barni, Volodymyr Savchenko
+# Gabriele Barni [UniGe]
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
