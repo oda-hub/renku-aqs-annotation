a228246875a5be50f38533715b7c61ab0719f1e3
burnout87 <gabriele.barni@gmail.com>
2023-11-06 11:16:32 +0100

    ignore nb2workflow and python version requirements

--- setup.py
+++ setup.py
@@ -43,6 +43,7 @@ setup(
     author='Gabriele Barni, Volodymyr Savchenko',
     author_email='',
     install_requires=install_requires,
+    python_requires='>=3.9,<3.12',
     packages=packages,
     entry_points={
         "renku": ["name_of_plugin = renkuaqsannotation.plugin"],
