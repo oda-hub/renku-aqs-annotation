00b1e68a58e422fb0e7d4a1a04acc0f2b6e157ef
burnout87 <gabriele.barni@gmail.com>
2023-11-08 14:07:35 +0100

    new Renku version

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ install_requires = [
     'pydotplus',
     'rdflib',
     'bs4',
-    'renku==2.6.0',
+    'renku==2.7.0',
     'matplotlib',
     'astroquery @ git+https://github.com/oda-hub/astroquery#egg=astroquery',
     'aqsconverters',
